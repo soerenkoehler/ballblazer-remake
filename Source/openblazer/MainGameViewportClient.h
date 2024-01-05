ad22a0befc10616f1419d96a6df8d8ebd2b7364b
Sören Köhler <soerenkoehler@users.noreply.github.com>
2024-01-05 19:00:21 +0100

    switch to c++ project

---
+++ Source/openblazer/MainGameViewportClient.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameViewportClient.h"
+#include "MainGameViewportClient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENBLAZER_API UMainGameViewportClient : public UGameViewportClient
+{
+	GENERATED_BODY()
+	
+};
